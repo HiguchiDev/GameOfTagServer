9cdf5f4cf927a4527acef6fad7d047a46d0e51b4
HiguchiDev <higuchi.dev@gmail.com>
2017-06-20 20:14:48 +0900

    bbbb

---
+++ WebSocketInspect/src/org/ukiuni/inspect/websocket/action/WebSocketEndpointAction.java
@@ -0,0 +1,48 @@
+package org.ukiuni.inspect.websocket.action;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/loadMessage")
+public class WebSocketEndpointAction {
+	public static List<Session> sessions = new ArrayList<Session>();
+
+	public static int test = 0;
+	@OnOpen
+	public void onOpen(Session session) {
+		// 開始時
+		sessions.add(session);
+	}
+
+	@OnMessage
+	public void onMessage(String message) throws IOException {
+		// クライアントからの受信時
+		test++;
+		message += " : " + String.valueOf(test);
+		for (Session session : sessions) {
+			session.getBasicRemote().sendText("{\"command\":\"message\", \"text\": \"" + message.replace("\\", "\\\\").replace("\"", "\\\"") + "\"}");
+		}
+	}
+
+	@OnError
+	public void onError(Throwable t) throws IOException {
+		// エラー発生時
+		for (Session session : sessions) {
+			session.getBasicRemote().sendText("{\"command\":\"error\", \"text\": \"" + t.getMessage().replace("\\", "\\\\").replace("\"", "\\\"") + "\"}");
+		}
+	}
+
+	@OnClose
+	public void onClose(Session session) {
+		// 完了時
+		sessions.remove(session);
+	}
+}
